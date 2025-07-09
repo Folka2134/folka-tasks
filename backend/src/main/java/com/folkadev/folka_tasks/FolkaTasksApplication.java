b84211afe50ae2da77c16e3f96ed590eee10b02a
folka2134 <folka2134@gmail.com>
2025-07-09 11:06:51 +0100

    chore: Initalized spring backend

---
+++ backend/src/main/java/com/folkadev/folka_tasks/FolkaTasksApplication.java
@@ -0,0 +1,13 @@
+package com.folkadev.folka_tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolkaTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolkaTasksApplication.class, args);
+	}
+
+}
