855ef0f6f3042a668e1c6989809548dea3d666e1
folka2134 <folka2134@gmail.com>
2025-07-09 12:54:32 +0100

    chore: Updated properties to work with postgres database

--- backend/src/test/java/com/folkadev/folka_tasks/FolkaTasksApplicationTests.java
+++ backend/src/test/java/com/folkadev/folka_tasks/FolkaTasksApplicationTests.java
@@ -2,12 +2,14 @@ package com.folkadev.folka_tasks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FolkaTasksApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
