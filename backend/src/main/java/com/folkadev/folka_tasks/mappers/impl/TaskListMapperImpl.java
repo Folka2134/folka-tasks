c59ca3bfe180d71ae4a0f006f32a34894054969e
folka2134 <folka2134@gmail.com>
2025-07-11 14:08:39 +0100

    feat: imeplemented TaskListMapperImpl

---
+++ backend/src/main/java/com/folkadev/folka_tasks/mappers/impl/TaskListMapperImpl.java
@@ -0,0 +1,49 @@
+package com.folkadev.folka_tasks.mappers.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.folkadev.folka_tasks.domain.entities.TaskStatus;
+import com.folkadev.folka_tasks.mappers.TaskMapper;
+import com.folkadev.folka_tasks.domain.dto.TaskListDto;
+import com.folkadev.folka_tasks.domain.entities.TaskList;
+import com.folkadev.folka_tasks.domain.entities.Task;
+import com.folkadev.folka_tasks.mappers.TaskListMapper;
+
+@Component
+public class TaskListMapperImpl implements TaskListMapper {
+
+  private final TaskMapper taskMapper;
+
+  public TaskListMapperImpl(TaskMapper taskMapper) {
+    this.taskMapper = taskMapper;
+  }
+
+  @Override
+  public TaskList fromDto(TaskListDto taskListDto) {
+
+    return new TaskList(taskListDto.title(), taskListDto.description(),
+        Optional.ofNullable(taskListDto.tasks()).map(tasks -> tasks.stream().map(taskMapper::fromDto).toList())
+            .orElse(null));
+  }
+
+  @Override
+  public TaskListDto toDto(TaskList taskList) {
+    return new TaskListDto(taskList.getId(), taskList.getTitle(), taskList.getDescription(),
+        Optional.ofNullable(taskList.getTasks()).map(List::size).orElse(0),
+        calculateTaskListProgress(taskList.getTasks()), Optional.ofNullable(taskList.getTasks())
+            .map(tasks -> tasks.stream().map(taskMapper::toDto).toList()).orElse(null));
+  }
+
+  private Double calculateTaskListProgress(List<Task> tasks) {
+    if (null == tasks) {
+      return null;
+    }
+    long closedTaskCount = tasks.stream().filter(task -> TaskStatus.CLOSED == task.getStatus()).count();
+
+    return (double) closedTaskCount / tasks.size();
+  }
+
+}
