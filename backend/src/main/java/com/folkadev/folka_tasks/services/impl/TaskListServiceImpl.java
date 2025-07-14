b4e4507bff66419425fce6ea0c9f301175fc8f45
folka2134 <folka2134@gmail.com>
2025-07-14 11:12:10 +0100

    chore: added missing service annotation

--- backend/src/main/java/com/folkadev/folka_tasks/services/impl/TaskListServiceImpl.java
+++ backend/src/main/java/com/folkadev/folka_tasks/services/impl/TaskListServiceImpl.java
@@ -2,10 +2,13 @@ package com.folkadev.folka_tasks.services.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.folkadev.folka_tasks.domain.entities.TaskList;
 import com.folkadev.folka_tasks.repositories.TaskListRepository;
 import com.folkadev.folka_tasks.services.TaskListService;
 
+@Service
 public class TaskListServiceImpl implements TaskListService {
 
   private final TaskListRepository taskListRepository;
