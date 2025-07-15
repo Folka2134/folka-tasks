4fe455ede6ca598d9b476b5c45908e2bbfd65cfa
folka2134 <folka2134@gmail.com>
2025-07-15 13:28:14 +0100

    chore: updated controller to handle routing

--- backend/src/main/java/com/folkadev/folka_tasks/controllers/TaskListController.java
+++ backend/src/main/java/com/folkadev/folka_tasks/controllers/TaskListController.java
@@ -1,13 +1,17 @@
 package com.folkadev.folka_tasks.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.folkadev.folka_tasks.domain.dto.TaskListDto;
-import com.folkadev.folka_tasks.mappers.TaskListMapper;
 import com.folkadev.folka_tasks.services.TaskListService;
 
 @Controller
@@ -15,16 +19,23 @@ import com.folkadev.folka_tasks.services.TaskListService;
 public class TaskListController {
 
   private final TaskListService taskListService;
-  private final TaskListMapper taskListMapper;
 
-  public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+  public TaskListController(TaskListService taskListService) {
     this.taskListService = taskListService;
-    this.taskListMapper = taskListMapper;
   }
 
   @GetMapping
   public List<TaskListDto> listTaskLists() {
-    return taskListService.listTaskList().stream().map(taskListMapper::toDto).toList();
+    return taskListService.listTaskList();
   }
 
+  @PostMapping
+  public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
+    return taskListService.createdTaskList(taskListDto);
+  }
+
+  @GetMapping(path = "/{task_list_id}")
+  public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListid) {
+    return taskListService.getTaskList(taskListid);
+  }
 }
