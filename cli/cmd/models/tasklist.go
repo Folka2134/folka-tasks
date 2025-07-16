45755fee5bd0002a31fccafe50cc55e57de899cd
folka2134 <folka2134@gmail.com>
2025-07-16 16:05:07 +0100

    chore: updated TaskList struct

--- cli/cmd/models/tasklist.go
+++ cli/cmd/models/tasklist.go
@@ -1,6 +1,16 @@
 package models
 
 type TaskList struct {
+	ID          string `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
+	Tasks       []Task `json:"tasks"`
+}
+
+type Task struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	Priority    string `json:"priority"`
 }
