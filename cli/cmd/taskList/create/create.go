20132425df8d071b4460cf07b6aa7e821dd1fbb8
folka2134 <folka2134@gmail.com>
2025-07-16 12:47:08 +0100

    feat: implemented create taskList function

--- cli/cmd/taskList/create/create.go
+++ cli/cmd/taskList/create/create.go
@@ -1,7 +1,12 @@
 package create
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	"github.com/folka2134/folka-tasks/cli/cmd/models"
 
 	"github.com/spf13/cobra"
 )
@@ -16,8 +21,7 @@ var createCmd = &cobra.Command{
 	Short: "Create a new task list",
 	Long:  "This command allows you to create a new task list. You can specify the task list details such as title, description, and due date.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Adding task list titled: %s\n", taskListTitle)
-		fmt.Printf("Description: %s\n", taskListDescription)
+		createTaskList(taskListTitle, taskListDescription)
 	},
 }
 
@@ -25,6 +29,44 @@ func init() {
 	createCmd.Flags().StringVarP(&taskListTitle, "title", "t", "", "Title of the task to be added")
 	createCmd.Flags().StringVarP(&taskListDescription, "description", "d", "", "Description of the task to be added")
 	createCmd.MarkFlagRequired("title")
+}
+
+func createTaskList(title, description string) {
+	if title == "" {
+		fmt.Println("Error: Title is required to create a task list.")
+		return
+	}
+
+	taskList := models.TaskList{
+		Title:       title,
+		Description: description,
+	}
+
+	jsonData, err := json.Marshal(taskList)
+	if err != nil {
+		fmt.Printf("Error marshalling task list: %v\n", err)
+		return
+	}
+
+	resp, err := http.Post("http://localhost:8080/task-lists", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Printf("Error creating task list: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		fmt.Printf("Failed to create task list. Status code: %d\n", resp.StatusCode)
+		return
+	}
+
+	var createdTaskList models.TaskList
+	if err := json.NewDecoder(resp.Body).Decode(&createdTaskList); err != nil {
+		fmt.Printf("Error decoding response: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Task list created successfully! Title: %s, Description: %s\n", createdTaskList.Title, createdTaskList.Description)
 }
 
 func GetCommand() *cobra.Command {
