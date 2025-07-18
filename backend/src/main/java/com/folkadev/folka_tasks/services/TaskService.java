13ce9920b6fae475f70b358bbcda4557311b9ac1
folka2134 <folka2134@gmail.com>
2025-07-18 13:33:19 +0100

    feat: implemented task service interface

---
+++ backend/src/main/java/com/folkadev/folka_tasks/services/TaskService.java
@@ -0,0 +1,16 @@
+package com.folkadev.folka_tasks.services;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import com.folkadev.folka_tasks.domain.dto.TaskDto;
+
+public interface TaskService {
+  TaskDto createTask(TaskDto taskDto);
+
+  Optional<TaskDto> getTask(UUID taskId);
+
+  Optional<TaskDto> updateTask(UUID taskId, TaskDto taskDto);
+
+  void deleteTask(UUID taskId);
+}
