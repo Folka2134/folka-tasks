7a3879a2a31444bce3e3b3cbe9968893af1e5caf
folka2134 <folka2134@gmail.com>
2025-07-22 14:08:55 +0100

    chore: updated use of Optional return

--- backend/src/main/java/com/folkadev/folka_tasks/services/TaskService.java
+++ backend/src/main/java/com/folkadev/folka_tasks/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.folkadev.folka_tasks.services;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -7,11 +8,12 @@ import com.folkadev.folka_tasks.domain.dto.TaskDto;
 
 public interface TaskService {
   List<TaskDto> listTasks();
+
   TaskDto createTask(TaskDto taskDto);
 
   Optional<TaskDto> getTask(UUID taskId);
 
-  Optional<TaskDto> updateTask(UUID taskId, TaskDto taskDto);
+  TaskDto updateTask(UUID taskId, TaskDto taskDto);
 
   void deleteTask(UUID taskId);
 }
