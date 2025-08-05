c9f9bf3e18ef61226c549f4c22d463ac73b3ad25
folka2134 <folka2134@gmail.com>
2025-08-05 14:07:10 +0100

    chore: updated depricated annotation

--- backend/src/test/java/com/folkadev/folka_tasks/controllers/TaskListControllerTest.java
+++ backend/src/test/java/com/folkadev/folka_tasks/controllers/TaskListControllerTest.java
@@ -25,10 +25,9 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.folkadev.folka_tasks.domain.dto.TaskListDto;
@@ -41,7 +40,7 @@ public class TaskListControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @MockBean
+  @MockitoBean
   private TaskListService taskListService;
 
   @Nested
